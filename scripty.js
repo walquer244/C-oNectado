542b8e51e8a5b312f53f7c699e8551d54898c399
walquer244 <isaac.sousa77@aluno.ce.gov.br>
2025-12-01 20:20:58 -0300

    site FINALIZADO

---
+++ scripty.js
@@ -0,0 +1,15 @@
+// let botoes = document.querySelectorAll(".botao");
+// botoes.forEach(function (botao) {
+//     botao.addEventListener("click", function () {
+//         let item = botao.closest(".item");
+//         let infor = item.querySelector(".infor");
+//         let icone = botao.querySelector(".icone");
+//         if (infor.classList.contains("aberto")) {
+//             infor.classList.remove("aberto");
+//             icone.classList.remove("rotated");
+//         } else {
+//             infor.classList.add("aberto");
+//             icone.classList.add("rotated");
+//         }
+//     })
+// });
